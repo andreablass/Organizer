0d95789494f5eb42bc6976474370c31cd96f4504
andrea <andreablass11@gmail.com>
2021-09-29 06:44:47 -0500

    firts scaffolds

---
+++ app/models/category.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class Category < ApplicationRecord
+    belongs_to:category
+end
